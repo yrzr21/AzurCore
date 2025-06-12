d36b9fccf32fc8143c12707f31735f85ca1dd767
toki <wangjingqi16@gmail.com>
2025-06-12 16:06:18 +0800

    通用组件 InputBar 支持多行

--- src/core/widget/input_bar.py
+++ src/core/widget/input_bar.py
@@ -1,39 +1,61 @@
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QLineEdit, QPushButton
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit
 
 
 class InputBar(QWidget):
     """
-    QLabel + QLineEdit + QPushButton
+    QLabel + QLineEdit/QTextEdit + QPushButton
     继续封装 InputBar 不是很明智（例如使用默认 handler），因为 handler 一般与业务逻辑紧耦合
     """
 
-    def __init__(self, label_text="", button_text="Click", input_changed_handler=None, button_handler=None):
+    def __init__(self, label_text="", button_text=None,
+                 input_changed_handler=None, button_handler=None,
+                 multiline=False):
         super().__init__()
         self.main_layout = QHBoxLayout(self)
 
+        # QLabel
         self.label = QLabel(label_text)
-        self.line_edit = QLineEdit()
-        self.button = QPushButton(button_text)
-
-        self.button.clicked.connect(button_handler)
-        self.line_edit.textChanged.connect(input_changed_handler)
-
         self.main_layout.addWidget(self.label)
-        self.main_layout.addWidget(self.line_edit)
-        self.main_layout.addWidget(self.button)
+
+        # QLineEdit/QTextEdit
+        self.multiline = multiline
+        if self.multiline:
+            self.input_widget = QTextEdit()
+        else:
+            self.input_widget = QLineEdit()
+        if input_changed_handler is not None:
+            self.input_widget.textChanged.connect(input_changed_handler)
+        self.main_layout.addWidget(self.input_widget)
+
+        # QPushButton
+        self.button = None
+        if button_text is not None:
+            self.button = QPushButton(button_text)
+            self.button.clicked.connect(button_handler)
+            self.main_layout.addWidget(self.button)
 
     # QLineEdit
     def text(self):
-        return self.line_edit.text()
+        if self.multiline:
+            return self.input_widget.toPlainText()
+        return self.input_widget.text()
 
     def setText(self, text):
-        self.line_edit.setText(text)
+        if self.multiline:
+            self.input_widget.setPlainText(text)
+        else:
+            self.input_widget.setText(text)
 
     def setPlaceholderText(self, text):
-        self.line_edit.setPlaceholderText(text)
+        if not self.multiline:
+            self.input_widget.setPlaceholderText(text)
+        # QTextEdit 没有 placeholder，可以拓展支持 QTextEdit placeholder hack
 
     def setEnabled(self, enabled):
-        self.line_edit.setEnabled(enabled)
+        self.input_widget.setEnabled(enabled)
+
+    def setReadOnly(self, readonly: bool = True):
+        self.input_widget.setReadOnly(readonly)
 
     # QLabel
     def setLabelText(self, text):
@@ -41,4 +63,5 @@ class InputBar(QWidget):
 
     # QPushButton
     def setButtonText(self, text):
-        self.button.setText(text)
+        if self.button:
+            self.button.setText(text)
